26c0e78c3b9fe1c885f0918bf2995b2f25867233
Aslan Mambetov <97795531+wizard0152@users.noreply.github.com>
2025-11-09 14:07:09 +0300

    hero section is complete

--- src/components/templates/MainPage/index.tsx
+++ src/components/templates/MainPage/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import cls from './style.module.scss';
 import { Button, Icon, Img, LinkText, Text, Title } from '@/components/ui';
 import { useDeviceType } from '@/hooks';
+import HeroSection from '@/components/modules/HeroSection';
 
 export const MainPage = (props: any) => {
 	const device = useDeviceType();
@@ -11,16 +12,7 @@ export const MainPage = (props: any) => {
 
 	console.log(device)
 	return (<>
-		<div className={cls.wrap}>
-			<Icon name='next' />
-			<h1>Next <span>TS</span></h1>
-			<p>device: {device}</p>
-			<Button >Связаться с нами</Button>
-			<Title>My Title</Title>
-			<Text size={1}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae beatae suscipit iure accusantium pariatur dolorum fugit minima, exercitationem quaerat. Alias.</Text>
-			<LinkText icon='mark'>hello</LinkText>
-			<Img src={'/images/stars.svg'} />
-		</div>
+		<HeroSection />
 	</>)
 }
 
